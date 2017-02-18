184983c71b16043d6161206ab06fe5d62065d190
Justin Kwitco <Justin Kwitco>
2017-02-18 17:20:33 -0500

    Commit 3

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello justin');
+
+//fuction to change text to upper case
+
+window.onload = function () {
+document.getElementById("button").onclick = function () {
+    //var up = document.getElementById("inputTxt"); 
+
+    //function upper() { 
+        console.log("hi");
+        //up.toUpperCase();
+   // }
+
+};
+};
