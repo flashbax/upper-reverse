4e2dfff2b3b8a7d150405899832e201b8f0b6005
flashbax <jkiwtco@gmail.com>
2017-02-20 13:59:15 -0500

    Commit 5

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 window.onload = function () {
-    
-    //function to change text to upper case
+
+    //function to reverse and change text to upper case
     document.getElementById("button").onclick = function () {
+        //get value of text box
         var str = document.getElementById("inputTxt").value; 
+        //convert to upper case and store in up
         var up = str.toUpperCase();
-        //console.log(up);
+        //get length of string from text box    
+        var len = str.length;
+        //display result in label
         document.getElementById("outTxt").innerHTML = up; 
+         
+        console.log(len); 
     };
+
+    
 };
