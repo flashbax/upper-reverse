00b1f3e23e69357bd44f86210ba572540e9a2c09
Justin Kwitco <Justin Kwitco>
2017-02-20 20:26:43 -0500

    Commit 11

--- index.js
+++ index.js
@@ -3,35 +3,23 @@ window.onload = function () {
     document.getElementById("button").onclick = function () {
     //get value of text box
     var str = document.getElementById("inputTxt").value;
-    
-        function cap(upit) { 
-            //convert to upper case and store in up
-            var up = str.toUpperCase();
-            return up;
-        };
             
+        //reverse
         function rev(revit) {  
             //get length of string from text box    
             var len = str.length;
             //create empty array for string
             var arr = []; 
-            //loop trough each char
+            //loop through each char
             for (var i = 0; i<=len; i++) { 
                 arr.push(str.charAt(len - i));
             }
+            //output all array elements as one
             return arr.join('');
         };
 
-        function make(makeit) { 
-            var revStr = rev(str);
-            var capStr = cap(str);
-            //combines functions to display both capital and reverse chars
-            var revcap = revStr+capStr;
-            return (revcap);
-        };
-
-        //put output into label
-        document.getElementById("outTxt").innerHTML = make(str);
+        //print output of reverse funtion to label and convert to uppercase
+        document.getElementById("outTxt").innerHTML = rev(str).toUpperCase();
     }; 
     
 };  
