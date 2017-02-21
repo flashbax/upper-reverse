eb3e00231c20364563adc7729903b552cd40af8c
Justin Kwitco <Justin Kwitco>
2017-02-20 20:18:24 -0500

    Commit 10

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
-window.onload = function () {
-     
-       
+window.onload = function () {      
    
     document.getElementById("button").onclick = function () {
     //get value of text box
     var str = document.getElementById("inputTxt").value;
     
-    function cap(e) { 
-         //convert to upper case and store in up
-         var up = str.toUpperCase();
-         return up;
-    };
-        
-    function rev(e) {  
-        //get length of string from text box    
-        var len = str.length;
-        //create empty array for string
-        var arr = []; 
-        //loop trough each char
-        for (var i = 0; i<=len; i++) { 
-            arr.push(str.charAt(len - i));
-        }
-        return arr.join('');
-    };
+        function cap(upit) { 
+            //convert to upper case and store in up
+            var up = str.toUpperCase();
+            return up;
+        };
+            
+        function rev(revit) {  
+            //get length of string from text box    
+            var len = str.length;
+            //create empty array for string
+            var arr = []; 
+            //loop trough each char
+            for (var i = 0; i<=len; i++) { 
+                arr.push(str.charAt(len - i));
+            }
+            return arr.join('');
+        };
 
-    function doit(e) { 
-        var revStr = rev(str);
-        var capStr = cap(str);
-        var revcap = revStr+capStr;
-        return (revcap);
-    };
+        function make(makeit) { 
+            var revStr = rev(str);
+            var capStr = cap(str);
+            //combines functions to display both capital and reverse chars
+            var revcap = revStr+capStr;
+            return (revcap);
+        };
 
-
-    
-        //document.getElementById("outTxt").innerHTML = do(str);
-        console.log(doit(str));  
-    };
-    
-        //console.log(cap(str)); 
-        //console.log(rev(str));
-        //document.getElementById("outTxt").innerHTML; 
-
+        //put output into label
+        document.getElementById("outTxt").innerHTML = make(str);
+    }; 
     
 };  
